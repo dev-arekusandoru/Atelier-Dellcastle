49aef97a2167bf7775344526aa7d8692a23a7dee
dev-arekusandoru <66217747+dev-arekusandoru@users.noreply.github.com>
2023-07-09 10:38:51 -0400

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+/** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors");
+
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    screens: {
+      sl: "370px",
+
+      sd: "480px",
+
+      sm: "640px",
+      // => @media (min-width: 640px) { ... }
+
+      md: "740px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      xl: "1280px",
+      // => @media (min-width: 1280px) { ... }
+
+      "2xl": "1536px",
+      // => @media (min-width: 1536px) { ... }
+    },
+    colors: {
+      ...colors,
+      offwhite: "#FFFBFC",
+      offblack: "#010400",
+      primary: "#30332E",
+    },
+  },
+  plugins: [require("tailwind-scrollbar-hide")],
+};
