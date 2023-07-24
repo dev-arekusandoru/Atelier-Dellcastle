50331f65611c729618091f1e27cccf695211051a
dev-arekusandoru <66217747+dev-arekusandoru@users.noreply.github.com>
2023-07-24 19:13:32 -0400

    improved nav and in-gallery nav

--- src/js/functions.js
+++ src/js/functions.js
@@ -23,3 +23,74 @@ function decideAnimationOnLoad(
     }
   });
 }
+
+function fadePageIn(pageBodyId) {
+  let pageBody = document.getElementById(pageBodyId);
+  let target = pageBodyId.split("-")[0];
+  let classes = pageBody.classList;
+
+  if (target == "home") {
+    classes.add("out-view");
+    classes.remove("in-view");
+    setTimeout(() => {
+      classes.add("hidden");
+    }, 700);
+    console.log("homePage");
+  } else if (classes.contains("out-view")) {
+    classes.add("in-view");
+    classes.remove("out-view");
+  }
+}
+function fadePageOut(pageBodyId, target) {
+  let pageBody = document.getElementById(pageBodyId);
+
+  let classes = pageBody.classList;
+
+  if (pageBodyId.split("-")[0] == "home") {
+    classes.remove("hidden");
+    setTimeout(() => {
+      classes.add("in-view");
+      classes.remove("out-view");
+      wind;
+    }, 25);
+  } else if (classes.contains("in-view")) {
+    classes.add("out-view");
+    classes.remove("in-view");
+  }
+  if (target.length > 0) {
+    setTimeout(() => {
+      window.location.href = target;
+    }, 500);
+  }
+}
+
+function getCurrentURL() {
+  let url = window.location.href;
+  let urlSplit = url.split("#");
+
+  if (urlSplit[1] && urlSplit[1].length != 0) {
+    return urlSplit[1];
+  }
+
+  return "nada";
+}
+
+function changeGalleryPage(pageTo) {
+  let pageFrom = localStorage.getItem("page"); // get current page id
+  let from = document.getElementById(pageFrom); // get the page
+  let to = document.getElementById(pageTo); // get the target page
+  localStorage.setItem("page", pageTo); // store the target page as current
+
+  let currentTab = document.getElementById(pageFrom + "-tab");
+  currentTab.classList.add("border-opacity-0");
+  let newTab = document.getElementById(pageTo + "-tab");
+  console.log(newTab);
+  newTab.classList.remove("border-opacity-0");
+
+  from.classList.remove("in-view");
+  from.classList.add("out-view");
+  setTimeout(() => {
+    to.classList.remove("out-view");
+    to.classList.add("in-view");
+  }, 200);
+}
